466818652d867726b9bf82065d7734e770cbec04
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2022-06-10 15:35:53 +0800

    added file input

--- src/com/csopesy/group1/Main.java
+++ src/com/csopesy/group1/Main.java
@@ -1,10 +1,34 @@
 package com.csopesy.group1;
 
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
 class Main {
-    int numberOfPassengers;
-    int numberOfCars;
+    static int numberOfPassengers;
+    static int numberOfCars;
+    static int capacityOfCars;
 
     static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Text file input: ");
+        String textFile = scan.nextLine();
+        File file = new File(textFile);
+        try {
+            Scanner fileScanner = new Scanner(file);
+            if(file.exists()){
+                String[] inputs = new String[0];
+                while(fileScanner.hasNextLine()){
+                    inputs = fileScanner.nextLine().split("");
+                }
+                numberOfPassengers = Integer.parseInt(inputs[0]);
+                capacityOfCars = Integer.parseInt(inputs[1]);
+                numberOfCars = Integer.parseInt(inputs[2]);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Number of Passengers: " + numberOfPassengers + " Capacity of Cars: " + capacityOfCars + " Number of Cars: " + numberOfCars);
     }
 }
