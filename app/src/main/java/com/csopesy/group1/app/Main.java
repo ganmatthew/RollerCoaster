1ac4e1500baab266883b6b3ba02bba46631b8d90
GianTee20 <giancarlo_tee@dlsu.edu.ph>
2022-06-14 00:21:27 +0800

    Fixed window size

--- app/src/main/java/com/csopesy/group1/app/Main.java
+++ app/src/main/java/com/csopesy/group1/app/Main.java
@@ -9,14 +9,21 @@ import java.io.IOException;
 
 public class Main extends Application {
     private final Controller controller = new Controller();
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 760;
 
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("display.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1009, 606);
         stage.setScene(scene);
         stage.show();
-
+        stage.setMinWidth(WIDTH);
+        stage.setMaxWidth(WIDTH);
+        stage.setMinHeight(HEIGHT);
+        stage.setMaxHeight(HEIGHT);
+        stage.setResizable(false);
         //controller.getInputFromTextFile();
     }
 
