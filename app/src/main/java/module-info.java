69b18c55fe0755a73025bbb130f569dfcb6aa626
Matthew Gan <24535419+ganmatthew@users.noreply.github.com>
2022-06-14 18:25:32 +0800

    Delete app directory

--- app/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.csopesy.group1.app {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.csopesy.group1.app to javafx.fxml;
-    exports com.csopesy.group1.app;
-}
