117f865c328a448ed0aa8df7f55288e5b47e0c06
ganmatthew <ganmatthew@users.noreply.github.com>
2022-06-14 18:36:12 +0800

    Refresh app directory

---
+++ app/src/main/java/com/csopesy/group1/app/Main.java
@@ -0,0 +1,39 @@
+package com.csopesy.group1.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    final boolean USE_GRAPHICAL_INTERFACE = true;
+    public final int WIDTH = 1000;
+    public final int HEIGHT = 760;
+
+    private final Controller controller = new Controller();
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("display.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1009, 606);
+        stage.setScene(scene);
+        stage.show();
+        stage.setMinWidth(WIDTH);
+        stage.setMaxWidth(WIDTH);
+        stage.setMinHeight(HEIGHT);
+        stage.setMaxHeight(HEIGHT);
+        stage.setResizable(false);
+        if (USE_GRAPHICAL_INTERFACE) {
+            controller.getFilenameFromView(scene);
+        } else {
+            controller.getFilenameFromCLI(scene);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
